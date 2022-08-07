7226f92cd712443a535aaac75cbcda11ecba0d33
Jihyeon Lee <78195241+Marline2@users.noreply.github.com>
2022-08-08 01:36:50 +0900

    add contents

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const list = document.getElementsByClassName('list');
+console.log(list);
+
+
+function change(list){
+  console.log(list);
+
+  // const active = document.getElementById("active");
+  // const no_active = document.getElementById("no_active");
+
+  // active.setAttribute('id', 'no_active');
+  // no_active.setAttribute('id', 'active');
+  // console.log(active, no_active);
+
+}
+
+
+
